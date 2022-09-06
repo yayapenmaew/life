08c0235b06a45b88a3c2db4c77fcbf1bbae84b0e
yayapenmaew <yylttwns@gmail.com>
2022-09-06 12:45:07 +0700

    Initial Commit

---
+++ Shared/lifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifeApp.swift
+//  Shared
+//
+//  Created by Parin Fungtammasan on 6/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct lifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
