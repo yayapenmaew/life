245e2d87e2d69abbc88ad4f3fe3768e74b7a9062
yayapenmaew <yylttwns@gmail.com>
2022-09-06 16:13:54 +0700

    second

--- Shared/MainView.swift
+++ Shared/MainView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct MainView: View {
+    let bgColor: Color = Color.pink
+    let name: String = "Yaya"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading) {
+                Text("Hello,")
+                Text("\(name) :-)")
+            }
+            Spacer()
+            VStack(alignment: .center) {
+                Text("here is your activities")
+                Divider()
+                Text("Activity")
+                Text("Expense")
+                Text("To-Dos")
+                Text("Food Diary")
+            }
+            .frame(height: 500.0)
+            .background(bgColor)
+            .cornerRadius(10)
+        }
     }
 }
 
